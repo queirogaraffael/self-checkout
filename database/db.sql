dd1de3b29349f7c42d9d58f48cafb0d583b2b0d0
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-14 12:39:53 -0300

    Refactor: melhorias finais

--- database/db.sql
+++ database/db.sql
@@ -7,6 +7,32 @@ CREATE TABLE categorias (
     nome VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE produtos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigoDeBarra VARCHAR(13) UNIQUE,
+    nome VARCHAR(40),
+    preco DOUBLE,
+    quantidade INT,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE vendas (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    dataHora DATETIME,
+    total DOUBLE
+);
+
+CREATE TABLE itemVenda (
+    venda_id INT NOT NULL,
+    produto_id BIGINT NOT NULL,
+    quantidade INT,
+    PRIMARY KEY (venda_id, produto_id),
+    FOREIGN KEY (venda_id) REFERENCES vendas(codigo),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
+
 INSERT INTO categorias (id, nome) VALUES 
     (1, 'Alimentos e Bebidas'),
     (2, 'Produtos de Limpeza'),
@@ -19,17 +45,6 @@ INSERT INTO categorias (id, nome) VALUES
     (9, 'Saúde e Bem-Estar'),
     (10, 'Automotivo'),
     (11, 'Outra');
-
-
-CREATE TABLE produtos (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    codigoDeBarra VARCHAR(13) UNIQUE,
-    nome VARCHAR(40),
-    preco DOUBLE,
-    quantidade INT,
-    categoria_id INT NOT NULL,
-    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
-);
 
 
 -- Inserir produtos de Alimentos e Bebidas
