c113d012754a88cc9e7e153dcc01d60a64c80c36
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2026-05-05 08:21:45 -0300

    feat: dividir scripts de banco de dados em schema.sql e data.sql

--- database/data.sql
+++ database/data.sql
@@ -1,37 +1,4 @@
-CREATE DATABASE estoque_fluxo_caixa_db;
-
 USE estoque_fluxo_caixa_db;
-
-CREATE TABLE categorias (
-    id INT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE produtos (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    codigoDeBarra VARCHAR(13) UNIQUE,
-    nome VARCHAR(40),
-    preco DOUBLE,
-    quantidade INT,
-    categoria_id INT NOT NULL,
-    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
-);
-
-CREATE TABLE vendas (
-    codigo INT AUTO_INCREMENT PRIMARY KEY,
-    dataHora DATETIME,
-    total DOUBLE
-);
-
-CREATE TABLE itemVenda (
-    venda_id INT NOT NULL,
-    produto_id BIGINT NOT NULL,
-    quantidade INT,
-    PRIMARY KEY (venda_id, produto_id),
-    FOREIGN KEY (venda_id) REFERENCES vendas(codigo),
-    FOREIGN KEY (produto_id) REFERENCES produtos(id)
-);
-
 
 INSERT INTO categorias (id, nome) VALUES 
     (1, 'Alimentos e Bebidas'),
